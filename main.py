a206de12247b395c59bad4cbd333af7849b13b8c
Tazkia Damayanti <tazkiadamayanti@gmail.com>
2024-12-10 22:56:50 +0700

    up

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+import nltk
+nltk.download('stopwords')
 
 # Load dataset
 df = pd.read_csv('dataset/dataset.csv')
