c17baf79afbc386788d37faf21d9e8b416e79274
Tazkia Damayanti <tazkiadamayanti@gmail.com>
2024-12-10 22:51:53 +0700

    up

--- App.py
+++ App.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 import pandas as pd
 from main import recommend_courses, sidebar, tfidf_matrix, df
 from sklearn.feature_extraction.text import TfidfVectorizer
+import nltk
 
 nltk.data.path.append('./nltk_data')
 
